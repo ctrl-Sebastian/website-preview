0409eeee1bfdbf274ee4f085df8155729cd2d135
Sebastian Marrera <smarrera@reispsolutions.com>
2024-12-04 14:29:11 -0400

    fix build

--- src/app/(pages)/marketplace/page.jsx
+++ src/app/(pages)/marketplace/page.jsx
@@ -226,12 +226,10 @@ export default function Marketplace() {
       <DialogBackdrop className="fixed inset-0 bg-black/30" />
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">{selectedProduct.name}</DialogTitle>
-            <Description>This will permanently deactivate your account</Description>
-            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
+            <DialogTitle className="font-bold">NAME</DialogTitle>
+            <Description>Descripcion del producto</Description>
             <div className="flex gap-4">
-              <button onClick={() => setIsOpen(false)}>Cancel</button>
-              <button onClick={() => setIsOpen(false)}>Deactivate</button>
+              <button onClick={() => setIsOpen(false)}>Cerrar</button>
             </div>
           </DialogPanel>
         </div>
